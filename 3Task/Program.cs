73febe89a20dcfa3301c9ddb882f9ae6af6ef29d
Semen Shevliuk <semenshevliuk@gmail.com>
2024-02-23 02:48:28 +1000

    Создал рекурсию для разворота массива

---
+++ 3Task/Program.cs
@@ -0,0 +1,10 @@
+﻿void ReversArray(int[] array, int first = 0)
+{
+    if(first != array.Length) {
+        ReversArray(array, first + 1);
+        Console.Write($"{array[first]} ");
+    }
+}
+
+int[] array  = {1, 5, 0, 10, 34 };
+ReversArray(array);
