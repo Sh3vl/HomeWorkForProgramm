325a0e6698d31479c72e303aee1006c1d8271005
Semen Shevliuk <semenshevliuk@gmail.com>
2024-02-23 03:00:15 +1000

    Сделал функцию аккермана

---
+++ 2Task/Program.cs
@@ -0,0 +1,16 @@
+﻿static int FunctionAkkerman(int n, int m)
+{
+    if (n < 0 && m < 0)
+    {
+        if (n == 0) return m + 1;
+        {
+                if (n == 0) return m + 1;
+        }
+    }
+  return FunctionAkkerman(n - 1, FunctionAkkerman(n, m - 1));
+}
+
+int n = 2;
+int m = 3;
+
+FunctionAkkerman(m, n);
