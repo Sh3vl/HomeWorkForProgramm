cdc96a3139853f11968e54cfcf44cd73a778587a
Semen Shevliuk <semenshevliuk@gmail.com>
2024-02-23 00:50:19 +1000

    Сделал первую задачу

--- 1Task/Program.cs
+++ 1Task/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿void ChangeNumbers(int m, int n)
+{
+if(n == m)
+    {
+        Console.Write(m);
+        return;
+    }
+    Console.Write(m  + " ");
+    ChangeNumbers(m + 1, n);
+}
+
+int n = 16;
+int m = 5;
+ChangeNumbers(m, n);
